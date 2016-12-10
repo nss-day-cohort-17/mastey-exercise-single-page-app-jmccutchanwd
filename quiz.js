3eaba32818265f25b4ed79c651cbcfda6aa65f56
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-09 23:11:55 -0600

    fixed images, adjusted JSON

--- quiz.js
+++ quiz.js
@@ -13,8 +13,8 @@ function loadInventory (callback) { // Load the inventory and send a callback fu
   var inventoryLoader = new XMLHttpRequest(); // invoked after the process is complete
   inventoryLoader.addEventListener("load", function (e) {
   loadInventory = JSON.parse(e.target.responseText);
-  console.log("load complete");
-  console.log("JSON", loadInventory);
+  console.log("load complete");//******************************************
+  console.log("JSON", loadInventory);//************************************
   populatePage(e);
   console.log(loadInventory.cars.length);
   });
@@ -29,7 +29,7 @@ function populatePage () {
   for(var i = 0; i < loadInventory.cars.length; i++){
     inventory += `<div class="col-lg-4 col-md-6">
                   <div class="card">
-                  <img class="card-img-top img-xs-center" src="images/red-mustang.png" alt="Card image cap">
+                  <img class="card-img-top img-xs-center" ${loadInventory.cars[i].img} alt="Card image cap">
                   <div class="card-block">
                     <h4 class="card-title">${loadInventory.cars[i].make}</h4>
                     <h5>${loadInventory.cars[i].year}</h5>
@@ -39,11 +39,12 @@ function populatePage () {
                   </div>
                   </div>
                   </div>  `
-
   }
-  console.log("Inventory", loadInventory);
+  console.log("Inventory", inventory);//***********************************
   document.getElementById("cardsGoHere").innerHTML = inventory;
+  // console.log(inventory);
 }
+
 //   // Loop over the inventory and populate the page
 
 //
@@ -54,3 +55,28 @@ function populatePage () {
 //   // Now that the DOM is loaded, establish all the event listeners needed
 //   activateEvents();
 // }
+
+/*
+
+src="images/05-crown-vic.png'
+src='images/05-mustang.png'
+src='images/06-rogue.png'
+src='images/09-corolla.png'
+src='images/11-rogue.png'
+src='images/14-mustang-gt.png'
+src='images/15-mustang-gt.png'
+src='images/19-ranger.png'
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
