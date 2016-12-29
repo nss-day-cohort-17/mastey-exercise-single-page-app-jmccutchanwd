b85fa1220d434719e0b433a52736bf6fd42e13d7
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-29 01:42:09 -0600

    fixed issues, all complete

--- quiz.js
+++ quiz.js
@@ -1,43 +1,37 @@
 /* ================================= */
-/* ====== By John McCutchan ======== */
+/* By John McCutchan =============== */
 /* ================================= */
 //
-console.log("1.Begin."); //**** Message ******************************************************1
-loadInventory();
+console.log("1.Begin."); //**** Message *********************************************************************************1
+loadInventory(); // load the JSON
 //
 /* ================================= */
-/* ====== Global Vars ============== */
+/* Global Vars ===================== */
 /* ================================= */
 var inventory = []; // container for inner html
 var tgtFocus; // container for .card data
 //
 /* ================================= */
-/* ====== Load JSON ================ */
+/* Load JSON ======================= */
 /* ================================= */
 function loadInventory (callback) { // Load the inventory
   var inventoryLoader = new XMLHttpRequest();
   inventoryLoader.addEventListener("load", function (e) { // Callback .......=
   loadInventory = JSON.parse(e.target.responseText);//.......................=
-  console.log("2.Load complete.");//...........................................=**** Message ****************************2
-  console.log("3.JSON: ", loadInventory);//....................................=**** Shows the JSON *********************3
+  console.log("2.Load complete.");//.........................................=**** Message ******************************2
+  console.log("3.JSON: ", loadInventory);//..................................=**** Shows the JSON ***********************3
   populatePage(e);//.........................................................=**** Calls the populatePage function
-  console.log("4.Number of cars in inventory: ",loadInventory.cars.length);//..=**** Number of cars in inventory ********4
+  console.log("5.Number of cars in inventory: ",loadInventory.cars.length);//=**** Number of cars in inventory ********5
   });//..................This is not called until after the section below is =
   inventoryLoader.open("GET", "https://car-sales-jm.firebaseio.com/.json"); // JSON Called from Firebase :)
-  inventoryLoader.send();
+  inventoryLoader.send(); // engage
 }
 //
-/* +++++++++ DEV TOOL ++++++++++++++ */
-// document.querySelector("body").addEventListener("click", function(e) {
-//   console.log(e);
-// });
-/* +++ REMOVE BEFORE PRODUCTION ++++ */
-//
 /* ================================= */
-/* ====== Create Cards ============= */
+/* Create Cards ==================== */
 /* ================================= */
-function populatePage () {
-  for(var i = 0; i < loadInventory.cars.length; i++){ // Loops through the JSON Parse to creat inner HTML
+function populatePage () { // called by loadInventory()
+  for(var i = 0; i < loadInventory.cars.length; i++){ // Loops through the JSON Parse to create inner HTML
     inventory += `<div class="col-lg-4 col-md-6">
                     <div class="card">
                       <img class="card-img-top img-fluid img-xs-center vis" ${loadInventory.cars[i].img} alt="Card image cap">
@@ -56,48 +50,62 @@ function populatePage () {
 }
 //
 /* ================================= */
-/* ====== Event Listeners ========== */
+/* Event Listeners ================= */
 /* ================================= */
-function activateEvents(e){
-  document.getElementById("cardsGoHere").addEventListener("click", focusCard);  // event listener to listen in inner HTML
-  document.getElementById("submitButton").addEventListener("click", buttonSubmit);
-  document.getElementById('modText').addEventListener("keyup", typeDescription);
-  console.log("5.Listeners turned on."); //**** Message **********************************************5
+function activateEvents(e){ // called by populatePage()
+  document.getElementById("cardsGoHere").addEventListener("click", focusCard);  // to listen in inner HTML
+  document.getElementById("modText").addEventListener("keyup", fillText); // to listen for key release in input field
+  console.log("4.Listeners turned on."); //**** Message *****************************************************************4
 }
 //
 /* ================================= */
-/* Focus When Crad Clicked ========= */
+/* Focus When Card Clicked ========= */
 /* ================================= */
-function focusCard(e){
-  var el = document.querySelector("div.card"); // focuses on DOM near div.card
-  tgtFocus = el.closest("div"); // looks for div closest to .vis in <img> (.card)
-  if (tgtFocus.id === ""){ // id attribute of .card
+function focusCard(e){ // called by clicking on card via listener above
+  tgtFocus = e.target; // focuses on DOM near div.card
+  document.getElementById("submitButton").blur(); // takes focus off of button
+  if (document.getElementById('focusStyle')) {
+          var oldFocus = document.getElementById('focusStyle');
+          oldFocus.id = "";// deselects previous selected
+  }
+  if ((tgtFocus.id === "") && (tgtFocus.className === "vis year") ||
+      (tgtFocus.id === "") && (tgtFocus.className === "vis card-title") ||
+      (tgtFocus.id === "") && (tgtFocus.className === "vis model") ||
+      (tgtFocus.id === "") && (tgtFocus.className === "vis price") ||
+      (tgtFocus.id === "") && (tgtFocus.className === "card-img-top img-fluid img-xs-center vis") ||
+      (tgtFocus.id === "") && (tgtFocus.className === "vis card-text")){ // id attribute of .card
     tgtFocus.id = "focusStyle"; // applies special styling focus to .card
-    document.getElementById('modText').focus(); // puts focus on text input
-    console.log("6.Card Clicked: ", tgtFocus, "ID: ",tgtFocus.id); //**** Message ****************************************6
     typeDescription(); // calls input text function
-  }else{ // if card already has Id of #focusStle, it gets removed
-    tgtFocus.id = ""; // clears focus if card is clicked again
-    document.getElementById('modText').blur(); // blurs input field
-    document.getElementById('modText').value = ""; // clears input field
   }
 }
 //
 /* ================================= */
-/* Type Description ================ */
+/* Transfer Type to Input ========== */
 /* ================================= */
-function typeDescription(e){
-  document.getElementById("modText").value = document.querySelector("p.card-text").innerHTML; // loads text from <p> to input
-  console.log("7.Whats in pFocus", document.getElementById("modText").value); //**** Message *************Trouble Shooting ********7
+function typeDescription(e){ // called by focusCard()
+  if((tgtFocus.id === "focusStyle") && (tgtFocus.className === "vis card-text")){ // makes sure you are on description block
+    document.getElementById("submitButton").addEventListener("click", buttonSubmit); // button listener
+    document.getElementById('modText').focus(); // puts focus on text input
+    document.getElementById("modText").value = tgtFocus.innerHTML; // transfers description to input field
+  }
+}
+//
+/* ================================= */
+/* Transfer Type to Page =========== */
+/* ================================= */
+function fillText(){ // called by keyup listener in input field
+  tgtFocus.innerHTML = document.getElementById("modText").value; // transfers input field changes to html
 }
 //
 /* ================================= */
 /* Submit Button =================== */
 /* ================================= */
-function buttonSubmit(e){
+function buttonSubmit(){ // called by button listener in typeDescription()
+  tgtFocus.innerHTML = document.getElementById("modText").value; // transfers input field changes to html, last time
   tgtFocus.id = ""; // clears focus style from card
-  document.getElementById('modText').blur(); // blurs input field
-  // document.getElementById('modText').value = ""; // clears input field
+  document.getElementById("modText").blur(); // blurs input field
+  document.getElementById("modText").value = ""; // clears input field
+  document.getElementById("submitButton").blur(); // takes focus off of button
 }
 //
 // END
